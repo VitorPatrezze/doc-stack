110a2e41f4cbacbc617bb4a2e6fa222467f80e44
Vitor Patrezze Nunes <74027841+VitorPatrezze@users.noreply.github.com>
2023-02-15 16:03:17 -0300

    testing

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_buvZpBUk4', // e.g. us-east-1_wBwgdLjXz
+        userPoolClientId: 'k2mkluc52qg18e7tcm0hppp91', // e.g. 7bql9c29vibdtpm2ahcm64sl28
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
