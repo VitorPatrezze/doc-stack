29f171ae8e8f91e370349a26167965233987085e
Vitor Patrezze Nunes <74027841+VitorPatrezze@users.noreply.github.com>
2023-02-17 17:36:48 -0300

    Test request 2

--- js/request.js
+++ js/request.js
@@ -2,7 +2,7 @@
 
 var authToken;
 
-(function requestScopeWrapper($) {
+function makeRequest(form) {
     authToken.then(function setAuthToken(token) {
         if (token) {
             authToken = token;
@@ -13,42 +13,30 @@ var authToken;
         alert(error);
         window.location.href = '/index.html';
     });
-    function makeRequest(form) {
-        $.ajax({
-            method: 'POST',
-            url: _config.api.invokeUrl + '/dev/test',
-            headers: {
-                Authorization: authToken
-            },
-            data: JSON.stringify(form),
-            contentType: 'application/json',
-            success: completeRequest,
-            error: function ajaxError(jqXHR, textStatus, errorThrown) {
-                console.error('Error posting form: ', textStatus, ', Details: ', errorThrown);
-                console.error('Response: ', jqXHR.responseText);
-                alert('An error occured when posting your form:\n' + jqXHR.responseText);
-            }
-        });
-    }
-
-    function completeRequest(result) {
-        console.log('Response received from API: ', result);
-    }
-
-    // Register click handler for #request button
-    $(function onDocReady() {
-        $('#request').click(handleRequestClick);
-        $(WildRydes.map).on('pickupChange', handlePickupChanged);
-
-        if (!_config.api.invokeUrl) {
-            $('#noApiMessage').show();
+    alert(authToken)
+    $.ajax({
+        method: 'POST',
+        url: _config.api.invokeUrl + '/dev/test',
+        headers: {
+            Authorization: authToken
+        },
+        data: JSON.stringify(form),
+        contentType: 'application/json',
+        success: completeRequest,
+        error: function ajaxError(jqXHR, textStatus, errorThrown) {
+            console.error('Error posting form: ', textStatus, ', Details: ', errorThrown);
+            console.error('Response: ', jqXHR.responseText);
+            alert('An error occured when posting your form:\n' + jqXHR.responseText);
         }
     });
+}
 
-    function handleRequestClick(event) {
-        alert(event)
-        event.preventDefault();
-        makeRequest(event);
+function handleRequestClick(form) {
+    var dict = {}
+    for (let i = 0; i < form.length; i++)
+    {
+        dict[form.elements[i].name] = form.elements[i].value
+        console.log(form.elements[i].value)
     }
-
-}(jQuery));
+    makeRequest(dict);
+}
