d625b17316aed9ddc77be0aceadf8fbd3400c45b
Vitor Patrezze Nunes <74027841+VitorPatrezze@users.noreply.github.com>
2023-02-17 18:41:56 -0300

    Test request 3

--- js/request.js
+++ js/request.js
@@ -1,35 +1,50 @@
-/*global WildRydes _config*/
+/*global _config*/
+import { Amplify, API } from 'aws-amplify';
+Amplify.configure(awsconfig);
 
+async function postData(form) {
+  const apiName = 'MyApiName';
+  const path = '/path';
+  const myInit = {
+    headers: {
+      Authorization: `Bearer ${(await Auth.currentSession())
+        .getIdToken()
+        .getJwtToken()}`
+    }
+  };
+
+  return await API.post(apiName, path, myInit);
+}
 var authToken;
 
-function makeRequest(form) {
-    authToken.then(function setAuthToken(token) {
-        if (token) {
-            authToken = token;
-        } else {
-            window.location.href = '/index.html';
-        }
-    }).catch(function handleTokenError(error) {
-        alert(error);
-        window.location.href = '/index.html';
-    });
-    alert(authToken)
-    $.ajax({
-        method: 'POST',
-        url: _config.api.invokeUrl + '/dev/test',
-        headers: {
-            Authorization: authToken
-        },
-        data: JSON.stringify(form),
-        contentType: 'application/json',
-        success: completeRequest,
-        error: function ajaxError(jqXHR, textStatus, errorThrown) {
-            console.error('Error posting form: ', textStatus, ', Details: ', errorThrown);
-            console.error('Response: ', jqXHR.responseText);
-            alert('An error occured when posting your form:\n' + jqXHR.responseText);
-        }
-    });
-}
+// function makeRequest(form) {
+//     authToken.then(function setAuthToken(token) {
+//         if (token) {
+//             authToken = token;
+//         } else {
+//             window.location.href = '/index.html';
+//         }
+//     }).catch(function handleTokenError(error) {
+//         alert(error);
+//         window.location.href = '/index.html';
+//     });
+//     alert(authToken)
+//     $.ajax({
+//         method: 'POST',
+//         url: _config.api.invokeUrl + '/dev/test',
+//         headers: {
+//             Authorization: authToken
+//         },
+//         data: JSON.stringify(form),
+//         contentType: 'application/json',
+//         success: completeRequest,
+//         error: function ajaxError(jqXHR, textStatus, errorThrown) {
+//             console.error('Error posting form: ', textStatus, ', Details: ', errorThrown);
+//             console.error('Response: ', jqXHR.responseText);
+//             alert('An error occured when posting your form:\n' + jqXHR.responseText);
+//         }
+//     });
+// }
 
 function handleRequestClick(form) {
     var dict = {}
@@ -38,5 +53,5 @@ function handleRequestClick(form) {
         dict[form.elements[i].name] = form.elements[i].value
         console.log(form.elements[i].value)
     }
-    makeRequest(dict);
+    postData(dict);
 }
