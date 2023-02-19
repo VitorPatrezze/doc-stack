14baf035ddf73788b28dc7e542fcae6b755f4488
Vitor Patrezze Nunes <74027841+VitorPatrezze@users.noreply.github.com>
2023-02-19 18:35:15 -0300

    Test request 11

--- js/button.js
+++ js/button.js
@@ -12,13 +12,25 @@ async function postData(form) {
   const path = '/dev/test';
   const accessToken = sessionStorage.getItem("access_token");
   const idToken = sessionStorage.getItem("id_token");
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://tmaqjddwt8.execute-api.us-east-1.amazonaws.com/dev/test");
-  xhr.setRequestHeader("Authorization", idToken);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  console.log("Teste");
-  console.log(xhr);
-  xhr.send(JSON.stringify(form));
+
+  fetch('https://tmaqjddwt8.execute-api.us-east-1.amazonaws.com/dev/test', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': idToken
+    },
+    body: JSON.stringify(form)
+  })
+    .then(response => console.log(JSON.stringify(response)))
+
+  // let xhr = new XMLHttpRequest();
+  // xhr.open("POST", "https://tmaqjddwt8.execute-api.us-east-1.amazonaws.com/dev/test");
+  // xhr.setRequestHeader("Authorization", idToken);
+  // xhr.setRequestHeader("Content-Type", "application/json");
+  // console.log("Teste");
+  // console.log(xhr);
+  // xhr.send(JSON.stringify(form));
+
   // const myInit = {
   //   headers: {
   //     Authorization: `Auth ${accessToken}`,
