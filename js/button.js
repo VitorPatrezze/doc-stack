1a5f2451e9cfac7fe8de9e597e8bcb046c3c4fe7
Vitor Patrezze Nunes <74027841+VitorPatrezze@users.noreply.github.com>
2023-02-19 11:14:37 -0300

    Test request 9

--- js/button.js
+++ js/button.js
@@ -12,7 +12,7 @@ async function postData(form) {
   const path = '/dev/test';
   const accessToken = sessionStorage.getItem("access_token");
   let xhr = new XMLHttpRequest();
-  xhr.open("POST", "/dev/test");
+  xhr.open("POST", "https://tmaqjddwt8.execute-api.us-east-1.amazonaws.com/dev/test");
   xhr.setRequestHeader("Authorization", accessToken)
   xhr.setRequestHeader("Accept", "application/json");
   xhr.setRequestHeader("Content-Type", "application/json");
