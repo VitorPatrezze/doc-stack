f11d0dc22dad6f75d0e1290ed1e43b386d18a5d5
Vitor Patrezze Nunes <74027841+VitorPatrezze@users.noreply.github.com>
2023-02-28 11:10:45 -0300

    updating form

--- js/consult.js
+++ js/consult.js
@@ -26,10 +26,14 @@ async function getData(uri) {
                 var cell2 = row.insertCell(1);
                 var cell3 = row.insertCell(2);
                 var cell4 = row.insertCell(3);
+                var cell5 = row.insertCell(4);
+                var cell6 = row.insertCell(5);
                 cell1.innerHTML = i;
                 cell2.innerHTML = rows[i]["diagnosis"];
                 cell3.innerHTML = rows[i]["anesthesia"];
                 cell4.innerHTML = rows[i]["duration"];
+                cell5.innerHTML = rows[i]["hospital-id"];
+                cell6.innerHTML = rows[i]["created-at"];
             }
         })
 }
