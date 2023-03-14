3f9f158f34199c557dc5669d3d156837772e3813
Vitor Patrezze Nunes <74027841+VitorPatrezze@users.noreply.github.com>
2023-03-13 22:10:14 -0300

    fixed access token

--- js/load-profile.js
+++ js/load-profile.js
@@ -8,7 +8,7 @@ async function getProfile() {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
-        'Auth': 'eyJraWQiOiJlcHBFd3lCVGlmNk5YUm5qUE5iVnlMM0VIQ25Ra2F4ak9FRzhOd2RSMVlFPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJiMmRkODIxMS1lMWMxLTQzNDAtYTU2Zi01MjNhNGMzYTQ3YjciLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9idXZacEJVazQiLCJ2ZXJzaW9uIjoyLCJjbGllbnRfaWQiOiJrMm1rbHVjNTJxZzE4ZTd0Y20waHBwcDkxIiwiZXZlbnRfaWQiOiJjMzgwYTdhMC03NzRmLTRiMzAtOTk5OS05ODhkMmI2ODE1ODEiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6Im9wZW5pZCBlbWFpbCIsImF1dGhfdGltZSI6MTY3ODc0ODkzOSwiZXhwIjoxNjc4NzUyNTM5LCJpYXQiOjE2Nzg3NDg5MzksImp0aSI6IjgyZWExMzNiLWNhM2MtNDYwMC1hNGYzLTY2ZmNhNzE0ZDRjNCIsInVzZXJuYW1lIjoidGVzdGUifQ.tFuXmC7E5M1Xt5iQuYsMqpxu_-38C-PpDiHJSZaD9j31PVvw4ayS0SsuSCKKffBGTe_Ac28gvUoP7dz8MZB7vmkpUqSOPKpyMFuNtPcdihdPn1QdnRAqOKTmnbkchpVXQD8OD8EfrdZh_TlRnzM12rRCra20kcf_ogUkF1jFijUlWKxXKhsMa1SKS32z-1RxEbb1XOywQMzW9ZGB2_2Xlj33TLHYcD_OsOYpA-ESr9uWK9UJfl0s-MLoUWjtItytJVuDgpwuaaU_26AET1VDkT4J-4G9DoyHbYwV0KtOcJldjoTaNoltpUECaoeGxoOBUvbCGH7p8KmAlWMIB2aaWg'
+        'Auth': accessToken
       }
     })
     .then(response => response.json())
